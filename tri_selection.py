7262a0eb55b19b531e6c78d483846eb3631355bc
Noah <noahcharleshenry4@gmail.com>
2022-11-23 17:02:18 +0100

    a

--- tri_selection.py
+++ tri_selection.py
@@ -24,4 +24,29 @@ def tri_selection(tab):
         tab[i], tab[min] = tab[min], tab[i] # On échange les deux éléments
     return tab # On retourne le tableau trié
    
-print(tri_selection(tab))
+print(tri_selection(tab)) 
+
+"""
+Ajoutez un compteur des comparaisons et un des échanges/affectations dans le tableau (un
+échange vaut 3 affectations) :
+"""
+
+tab = [1,10,291,2829,10,2903,20,9300,0,20]
+def tri_selection2(tab):
+    nb_compa = 0
+    nb_echange = 0
+    for i in range(len(tab)): # On parcourt le tableau
+        nb_compa += 1
+        min = i # On considère que le minimum est le premier élément
+        for j in range(i+1, len(tab)): # On parcourt le reste du tableau
+            nb_compa += 1
+            if tab[j] < tab[min]: # Si on trouve un élément plus petit
+                min = j # On le considère comme nouveau minimum
+        tab[i], tab[min] = tab[min], tab[i] # On échange les deux éléments
+        nb_echange += 3
+    print("Le nombre de comparaison est de : ", nb_compa, "et le nombre d'échange est de : ", nb_echange)
+    return tab
+
+
+   
+print(tri_selection2(tab))

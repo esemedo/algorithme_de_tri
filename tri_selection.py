5cc5f55146cb72d31b48b474e6e9735feaf984e6
Noah <noahcharleshenry4@gmail.com>
2022-11-23 16:33:51 +0100

    version1 algo tri-selection

--- tri_selection.py
+++ tri_selection.py
@@ -1,3 +1,19 @@
+
+""""
+Algorithme de tri par sélection
+
+procédure tri_selection(tableau t)
+      n ← longueur(t) 
+      pour i de 0 à n - 2
+          min ← i       
+          pour j de i + 1 à n - 1
+              si t[j] < t[min], alors min ← j
+          fin pour
+      fin pour
+  fin procédure
+"""
+
+
 tab = [1,10,291,2829,10,2903,20,9300,0,20]
 def tri_selection(tab):
     for i in range(len(tab)): # On parcourt le tableau
